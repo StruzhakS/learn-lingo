bd7d1b5d0e4e079e04228b87cfcf992768198bb2
StruzhakS <StruzhakS@gmail.com>
2023-11-01 12:42:52 +0200

    Update App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">Learn-Lingo</div>;
+  return <div className="App">Learn Lingo</div>;
 }
 
 export default App;
