1221e028e0388f424abe2b692a1e49d9e565204d
StruzhakS <StruzhakS@gmail.com>
2023-11-10 08:29:01 +0200

    update TeacherCard

--- src/Components/TeacherCard/TeacherCard.jsx
+++ src/Components/TeacherCard/TeacherCard.jsx
@@ -2,7 +2,30 @@ import React from 'react';
 import { TeacherItem } from './TeacherCard.styled';
 
 const TeacherCard = ({ item, index }) => {
-  return <TeacherItem>TeacherCard</TeacherItem>;
+  console.log(item);
+  return (
+    <TeacherItem>
+      <div>
+        <img src={item.avatar_url} alt={`avatar ${item.name} `} width="80" height="80" />
+      </div>
+      <div>
+        <p>languages</p>
+        <h2>{item.name}</h2>
+        <p>
+          <span>Speak:</span> {item.languages.join(', ')}
+        </p>
+        <p>
+          <span>Lesson info:</span> {item.lesson_info}
+        </p>
+        <p>
+          <span>Conditions:</span> {item.conditions}
+        </p>
+        {item.levels.map((level, idx) => (
+          <input key={idx} type="radio" id={level} name={level} value={idx} checked />
+        ))}
+      </div>
+    </TeacherItem>
+  );
 };
 
 export default TeacherCard;
